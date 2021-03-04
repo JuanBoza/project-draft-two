d09c342d5927bf1c464e0af2f4e9388a14957cf2
Juan Boza <Juanboza80@gmail.com>
2021-03-03 19:33:04 -0700

    files added

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Cards from './components/cards';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Cards /> 
+    <main className="main">
+        
+      
+    </main> 
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App /> {/* The various pages will be displayed by the `Main` component. */}
+  </BrowserRouter>
+  ), document.getElementById('root')
+);
+
